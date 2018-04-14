92a7917141b808067109404501c8768f3962bf69
Kai Brabo <kaibrabo@gmail.com>
2018-04-14 21:36:46 +0000

    post#create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,23 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+    
+    if @post.save
+      flash[:notice] = 'Post was saved!'
+      redirect_to @post
+    else
+      flash.now[:alert] = "Something went wrong, please try again..."
+      render :new
+    end
+  end
+  
   def edit
   end
 end
